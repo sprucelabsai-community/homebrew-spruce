638d464fabb7063f5d18a32374c552a7018e3335
Taylor Romero <me@taylorrome.ro>
2024-09-05 10:10:51 -0600

    patch: test

--- spruce-cli.rb
+++ spruce-cli.rb
@@ -2,12 +2,15 @@ class SpruceCli < Formula
     require "language/node"
     desc "The SpruceCLI is a command line tool for working with Spruce Platform."
     homepage "https://github.com/sprucelabsai-community/spruce-cli-workspace"
+    url "https://registry.npmjs.org/@sprucelabs/spruce-cli/-/spruce-cli-latest.tgz"
     license "MIT"
   
     livecheck do
       url "https://registry.npmjs.org/@sprucelabs/spruce-cli/latest"
       regex(/"version":\s*"?(\d+(?:\.\d+)+)"?/i)
     end
+  
+    depends_on "node"
   
     def install
       system "npm", "install", "-g", "@sprucelabs/spruce-cli@latest"
